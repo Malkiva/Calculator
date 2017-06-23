5d681d943433c0a35acc3d711622f7e8d90814bd
Malkiva <malakhov.ivan3712@yandex.ru>
2017-06-23 22:29:59 +0300

    JS+CSS

---
+++ js.js
@@ -0,0 +1,30 @@
+jQuery('document').ready(function(){
+	
+$('button').on('click', function(){
+	
+	var first , second , result ;
+	
+	first = $("#first").val();
+	second = $("#second").val();
+    
+	first = parseInt(first);
+    second = parseInt(second);	
+	if ($(this).attr('id')=='plus') { 
+     result = first + second; 
+    }
+    else if ($(this).attr('id')=='multiply'){ 
+     result = first * second; 
+    }
+    else if ($(this).attr('id')=='minus'){ 
+     result = first - second; 
+    }
+    else if ($(this).attr('id')=='delenie'){ 
+     result = first / second;
+    }
+		
+ $("#result").val(result);
+})
+	
+	
+    
+})
