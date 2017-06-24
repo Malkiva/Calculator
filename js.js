f52b89f4ecd765906210ed9f0c27d96e0edce2cb
Malkiva <malakhov.ivan3712@yandex.ru>
2017-06-24 13:28:04 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,25 +1,22 @@
 $(function(){
 	
-$('button').on('click', function(){
+    $('button').on('click', function(){
 	
-        var first = parseInt($("#first").val());
+	var first = parseInt($("#first").val());
 	var second = parseInt($("#second").val());
 	
 	if ($(this).attr('id')=='plus') { 
-     result = first + second; 
-    }
-    else if ($(this).attr('id')=='multiply'){ 
-     result = first * second; 
-    }
-    else if ($(this).attr('id')=='minus'){ 
-     result = first - second; 
-    }
-    else if ($(this).attr('id')=='delenie'){ 
-     result = first / second;
-    }
+            result = first + second; 
+        } else if ($(this).attr('id')=='multiply'){ 
+            result = first * second; 
+        } else if ($(this).attr('id')=='minus'){ 
+            result = first - second; 
+        } else if ($(this).attr('id')=='delenie'){ 
+            result = first / second;
+        }
 		
- $("#result").val(result);
-})
+        $("#result").val(result);
+    })
 	
 	
     
