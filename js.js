cd0da145051a8666b4ce9adf7a07c3a3ae232c80
Malkiva <malakhov.ivan3712@yandex.ru>
2017-06-25 15:51:54 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,23 +1,31 @@
-$(function(){
-	
-    $('button').on('click', function(){
-	
-	var first = parseInt($("#first").val());
-	var second = parseInt($("#second").val());
-	
-	if ($(this).attr('id')==='plus') { 
-            result = first + second; 
-        } else if ($(this).attr('id')==='multiply'){ 
-            result = first * second; 
-        } else if ($(this).attr('id')==='minus'){ 
-            result = first - second; 
-        } else if ($(this).attr('id')==='delenie'){ 
-            result = first / second;
+$(function() {
+
+    $('button').on('click', function() {
+
+        var first = parseInt($("#first").val());
+        var second = parseInt($("#second").val());
+        var result;
+        var op = $(this).attr('id');
+        switch (op) {
+            case 'plus':
+                result = first + second;
+                break;
+            case 'minus':
+                result = first - second;
+                break;
+            case 'multiply':
+                result = first * second;
+                break;
+            case 'delenie':
+                if (second === 0) {
+                    $("#result").val("На ноль делить нельзя");
+                    return;
+                }
+                result = first / second;
+                break;
         }
-		
         $("#result").val(result);
     })
-	
-	
-    
+
+
 })
